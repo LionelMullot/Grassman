af9749bf801b677fe23927e2069f1f7498ae1ca2
Jonathan Douet <jonathandouet@hotmail.fr>
2013-12-03 22:05:43 +0100

    Methodes pour un quadvector

--- lib/include/GCA_quadvector.hpp
+++ lib/include/GCA_quadvector.hpp
@@ -1,12 +1,30 @@
 #pragma once
 
-#include <Eigen/Dense>
-#include <Eigen/QR>
+#include <iostream>
 
 namespace gca{
 
-class GCA_quadvector: public Eigen::VectorXd{
-	GCA_quadvector();
+class GCA_antiscalar;
+
+class GCA_quadvector{
+public:
+    //Constructeur
+    GCA_quadvector();
+    GCA_quadvector(const GCA_quadvector& other);
+    GCA_quadvector(const double value);
+
+    double& getValue();
+
+    GCA_quadvector& operator=(const GCA_quadvector& Other);
+    GCA_quadvector& operator<<(const double& in);
+
+    GCA_antiscalar& operator~();
+
+
+private:
+    double value;
 };
 
+std::ostream& operator<<(std::ostream& os, GCA_quadvector& quadvector);
 }
+
--- lib/src/GCA_quadvector.cpp
+++ lib/src/GCA_quadvector.cpp
@@ -1,1 +1,30 @@
 #include "GCA_quadvector.hpp"
+#include <iostream>
+
+gca::GCA_quadvector::GCA_quadvector():
+    value(1){}
+
+gca::GCA_quadvector::GCA_quadvector(const gca::GCA_quadvector& other):
+    value(other.value){}
+
+gca::GCA_quadvector::GCA_quadvector(const double value):
+    value(value){}
+
+double& gca::GCA_quadvector::getValue(){
+    return value;
+}
+
+gca::GCA_quadvector& gca::GCA_quadvector::operator=(const gca::GCA_quadvector& Other){
+    this->value = Other.value;
+}
+
+gca::GCA_quadvector& gca::GCA_quadvector::operator<<(const double& in){
+    this->value = in;
+}
+
+gca::GCA_antiscalar& gca::GCA_quadvector::operator~(){}
+
+std::ostream& gca::operator<<(std::ostream& os, gca::GCA_quadvector& quadvector){
+  os << quadvector.getValue();
+  return os;
+}
